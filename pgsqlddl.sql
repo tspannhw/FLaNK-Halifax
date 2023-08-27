8b3c3e3cec72c543c63032d39da5a071acea1e5c
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2023-08-26 23:57:32 -0400

    Create pgsqlddl.sql

---
+++ pgsqlddl.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.halifaxlookuproutes
+(route_id, agency_id, route_short_name, route_long_name, route_desc, route_type, route_url, route_color, route_text_color)
+VALUES('ecrl', 'ecrl', 'ecrl', 'ecrl', NULL, '3', NULL, '000000', '000000');
